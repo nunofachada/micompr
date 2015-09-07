d87331ff86156cdc06e0746962951b593c113478
Nuno Fachada <nfachada@laseeb.org>
2015-09-07 12:04:09 +0100

    Add plot capabilities to compare_models

--- compare_models.R
+++ compare_models.R
@@ -74,7 +74,7 @@ compare_models <- function(ve, data, factors) {
   }
   
   # Return
-  cmpmod <- list(scores=pca$x, varexp=varexp, npcs=npcs, ve=ve, 
+  cmpmod <- list(scores=pca$x, factors=factors, varexp=varexp, npcs=npcs, ve=ve, 
                  p.values=list(manova=mnvpval, parametric=parpvals, nonparametric=nonparpvals),
                  tests=list(manova=mnvtest, parametric=partests, nonparametric=nonpartests))
   class(cmpmod) <- "cmpmodels"
@@ -83,6 +83,7 @@ compare_models <- function(ve, data, factors) {
 }
 
 print.cmpmodels <- function(cmpmod) {
+
   cat("Number of PCs which explain ", cmpmod$ve * 100, "% of variance: ", cmpmod$npcs, "\n", sep="")
   if (cmpmod$npcs > 1) {
     cat("P-Value for MANOVA along", cmpmod$npcs, "dimensions:", cmpmod$p.values$manova, "\n")
@@ -90,4 +91,28 @@ print.cmpmodels <- function(cmpmod) {
   cat("P-Value for t-test (1st PC):", cmpmod$p.values$parametric[1], "\n")
   cat("P-Value for Mann-Whitney test (1st PC):", cmpmod$p.values$parametric[2], "\n")
 
+}
+
+plot.cmpmodels <- function(cmpmod, col=c("blue","red","green","gold","violet","cyan"), ...) {
+  
+  par(mfrow=c(2,2))
+  
+  # Score plot (first two PCs)
+  plot.default(cmpmod$scores[,1], cmpmod$scores[,2], col=col[as.numeric(cmpmod$factors)], 
+               xlab=paste("PC1 (", round(cmpmod$varexp[1] * 100, 2), "%)", sep = ""), 
+               ylab=paste("PC2 (", round(cmpmod$varexp[2] * 100, 2), "%)", sep = ""), 
+               main="Score plot", ...)
+  
+  # Explained variance bar plot
+  barplot(cmpmod$varexp[1:cmpmod$npcs], names.arg=as.character(1:cmpmod$npcs), 
+          main="Explained variance by PC", xlab="PC", ylab="Var. exp. (%)", ...)
+  
+  # Parametric p-values bar plot
+  barplot(cmpmod$p.values$parametric, names.arg=as.character(1:cmpmod$npcs), 
+          main="Parametric p-values by PC", xlab="PC", ylab="Prob.", ...)
+  
+  # Non-parametric p-values bar plot
+  barplot(cmpmod$p.values$nonparametric, names.arg=as.character(1:cmpmod$npcs), 
+          main="Non-parametric p-values by PC", xlab="PC", ylab="Prob.", ...)
+  
 }
