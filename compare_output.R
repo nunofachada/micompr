79b47807766cbb3f2f343f412801b6b0a09d96a6
Nuno Fachada <nfachada@laseeb.org>
2015-09-07 17:02:56 +0100

    Rename compare_models to compare_output, add output name

--- compare_output.R
+++ compare_output.R
@@ -1,14 +1,15 @@
 #' Title
 #'
+#' @param name
 #' @param ve 
 #' @param data 
-#' @param groups 
+#' @param factors 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-compare_models <- function(ve, data, factors) {
+compare_output <- function(name, ve, data, factors) {
 
   # Check parameters
   if (ve < 0 || ve > 1) 
@@ -74,45 +75,46 @@ compare_models <- function(ve, data, factors) {
   }
   
   # Return
-  cmpmod <- list(scores=pca$x, factors=factors, varexp=varexp, npcs=npcs, ve=ve, 
+  cmpout <- list(scores=pca$x, factors=factors, varexp=varexp, npcs=npcs, ve=ve, name=name,
                  p.values=list(manova=mnvpval, parametric=parpvals, nonparametric=nonparpvals),
                  tests=list(manova=mnvtest, parametric=partests, nonparametric=nonpartests))
-  class(cmpmod) <- "cmpmodels"
-  cmpmod
+  class(cmpout) <- "cmpoutput"
+  cmpout
   
 }
 
-print.cmpmodels <- function(cmpmod) {
+print.cmpoutput <- function(cmpout) {
 
-  cat("Number of PCs which explain ", cmpmod$ve * 100, "% of variance: ", cmpmod$npcs, "\n", sep="")
-  if (cmpmod$npcs > 1) {
-    cat("P-Value for MANOVA along", cmpmod$npcs, "dimensions:", cmpmod$p.values$manova, "\n")
+  cat("Output name:", cmpout$name, "\n")
+  cat("Number of PCs which explain ", cmpout$ve * 100, "% of variance: ", cmpout$npcs, "\n", sep="")
+  if (cmpout$npcs > 1) {
+    cat("P-Value for MANOVA along", cmpout$npcs, "dimensions:", cmpout$p.values$manova, "\n")
   }
-  cat("P-Value for t-test (1st PC):", cmpmod$p.values$parametric[1], "\n")
-  cat("P-Value for Mann-Whitney test (1st PC):", cmpmod$p.values$parametric[2], "\n")
+  cat("P-Value for t-test (1st PC):", cmpout$p.values$parametric[1], "\n")
+  cat("P-Value for Mann-Whitney test (1st PC):", cmpout$p.values$parametric[2], "\n")
 
 }
 
-plot.cmpmodels <- function(cmpmod, col=c("blue","red","green","gold","violet","cyan"), ...) {
+plot.cmpoutput <- function(cmpout, col=c("blue","red","green","gold","violet","cyan"), ...) {
   
   par(mfrow=c(2,2))
   
   # Score plot (first two PCs)
-  plot.default(cmpmod$scores[,1], cmpmod$scores[,2], col=col[as.numeric(cmpmod$factors)], 
-               xlab=paste("PC1 (", round(cmpmod$varexp[1] * 100, 2), "%)", sep = ""), 
-               ylab=paste("PC2 (", round(cmpmod$varexp[2] * 100, 2), "%)", sep = ""), 
+  plot.default(cmpout$scores[,1], cmpout$scores[,2], col=col[as.numeric(cmpout$factors)], 
+               xlab=paste("PC1 (", round(cmpout$varexp[1] * 100, 2), "%)", sep = ""), 
+               ylab=paste("PC2 (", round(cmpout$varexp[2] * 100, 2), "%)", sep = ""), 
                main="Score plot", ...)
   
   # Explained variance bar plot
-  barplot(cmpmod$varexp[1:cmpmod$npcs], names.arg=as.character(1:cmpmod$npcs), 
+  barplot(cmpout$varexp[1:cmpout$npcs], names.arg=as.character(1:cmpout$npcs), 
           main="Explained variance by PC", xlab="PC", ylab="Var. exp. (%)", ...)
   
   # Parametric p-values bar plot
-  barplot(cmpmod$p.values$parametric, names.arg=as.character(1:cmpmod$npcs), 
+  barplot(cmpout$p.values$parametric, names.arg=as.character(1:cmpout$npcs), 
           main="Parametric p-values by PC", xlab="PC", ylab="Prob.", ...)
   
   # Non-parametric p-values bar plot
-  barplot(cmpmod$p.values$nonparametric, names.arg=as.character(1:cmpmod$npcs), 
+  barplot(cmpout$p.values$nonparametric, names.arg=as.character(1:cmpout$npcs), 
           main="Non-parametric p-values by PC", xlab="PC", ylab="Prob.", ...)
   
 }
