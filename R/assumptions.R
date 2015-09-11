518c3584a68b779859f39f3503be757823fb07b6
Nuno Fachada <nfachada@laseeb.org>
2015-09-11 15:34:31 +0100

    Implement multivar. assumptions (norm. + homogen. cov. matrices)

--- R/assumptions.R
+++ R/assumptions.R
@@ -9,11 +9,27 @@
 #' @examples #' todo
 assumptions_manova <- function(data, factors) {
   
+  # `assumpt` will be a list containing the test for multivariate normality 
+  # and the test for the homogeneity of covariance matrices
   assumpt = list()
   assumpt$mvntest <- list()
+  assumpt$vartest <- NULL
+
+  # Number of variables
+  nvars <- dim(data)[2]
+  
+  # Cycle through factors for the multivariate normality tests
   for (f in unique(factors)) {
-    assumpt$mvntest[[f]] <- MVN::roystonTest(data[factors==f,])
+    # Indexes of current factor level
+    fidx <- factors==f
+    nobs <- sum(fidx)
+    assumpt$mvntest[[f]] <- MVN::roystonTest(data[fidx,1:min(nobs-1, nvars)])
   }
+  
+  # Perform the homogeneity of covariance matrices test (Box's M)
+  maxvars <- min(table(factors)-1, nvars)
+  assumpt$vartest <- biotools::boxM(data[,1:maxvars], factors)
+  
   assumpt
 }
 
@@ -34,7 +50,6 @@ assumptions_paruv <- function(data, factors) {
   
   # `assumpt` will be a list containing the tests for univariate normality 
   # assumptions and the test for the homogeneity of variances assumption.
-  # One test per variable in both cases.
   assumpt = list()
   assumpt$uvntest <- list()
   assumpt$vartest <- list()
