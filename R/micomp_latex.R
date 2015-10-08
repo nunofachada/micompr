a014f3ee814a0ce17e20bbce56f4fd27da23c54b
Nuno Fachada <faken@fakenmc.com>
2015-10-08 20:59:47 +0100

    Additional doccomments to micomp_latex.R (wip)

--- R/micomp_latex.R
+++ R/micomp_latex.R
@@ -138,12 +138,46 @@ tikzscat <- function(data, factors, marks, tscale, axes.color = "gray") {
 
 }
 
-tscat_apply <- function(cmps, marks, tscale) {
+#' Multiple \code{TikZ} 2D scatter plots for a list of output comparisons.
+#'
+#' Produce multiple \code{TikZ} 2D scatter plots for a list of
+#' \code{\link{cmpoutput}} objects.
+#'
+#' This function is mainly to be used by the \code{\link{toLatex.micomp}}
+#' method.
+#'
+#' @param cmps List of \code{\link{cmpoutput}} objects.
+#' @param marks Character vector determining how to draw the points in
+#' \code{TikZ}, for example: \code{
+#' c("mark=square*,mark options={color=red},mark size=0.8pt",
+#'   "mark=*,mark size=0.6pt",
+#'   "mark=o,mark size=0.7pt")}.
+#' @param tscale The \code{scale} property of the \code{TikZ} figure.
+#' @param before \code{LaTeX} code to paste before each \code{TikZ} figure.
+#' @param after \code{LaTeX} code to paste after each \code{TikZ} figure.
+#'
+#' @return List of TikZ 2D scatter plots corresponding to the comparisons
+#' provided in \code{cmps}.
+#'
+#' @export
+#' @keywords internal
+#'
+#' @examples
+#' NULL
+tscat_apply <- function(cmps, marks, tscale, before = "", after = "") {
 
+  # Get data and factors to produce figures
   scores <- lapply(cmps, function(x) x$scores)
   factors <- lapply(cmps, function(x) x$factors)
+
+  # Produce figures
   plts <- mapply(tikzscat, scores, factors, MoreArgs = list(marks, tscale))
-  paste("\\raisebox{-.5\\height}{\\resizebox {1.2cm} {1.2cm} {", plts, "}}")
+
+  # Wrap figures with before and after latex code
+  figs <- paste(before, plts, after)
+
+  # Return TikZ figures
+  figs
 
 }
 
@@ -154,11 +188,16 @@ tscat_apply <- function(cmps, marks, tscale) {
 #' @param table.placement
 #' @param latex.environments
 #' @param booktabs
-#' @param btalign
+#' @param booktabs.cmalign
+#' @param caption
+#' @param label
 #' @param col.width
 #' @param digits
 #' @param pvalformat
-#' @param marks
+#' @param scoreplot.marks
+#' @param scoreplot.scale
+#' @param scoreplot.before
+#' @param scoreplot.after
 #' @param ...
 #'
 #' @return todo
@@ -166,138 +205,142 @@ tscat_apply <- function(cmps, marks, tscale) {
 #'
 #' @examples
 #' NULL
-toLatex.micomp <-
-  function(mic,
-           data.show = c("npcs", "mnvp", "parp", "nparp", "scoreplot"),
-           table.placement = "ht",
-           latex.environments = c("center"),
-           booktabs = F,
-           booktabs.cmalign = "l",
-           caption = NULL,
-           label = NULL,
-           col.width = F,
-           digits = 3,
-           pvalformat = pvalf,
-           scoreplot.marks = c(
-             "mark=square*,mark options={color=red},mark size=0.8pt",
-             "mark=*,mark size=0.6pt",
-             "mark=o,mark size=0.7pt"),
-           scoreplot.scale = 6,
-           ...) {
+toLatex.micomp <- function(
+  mic,
+  data.show = c("npcs", "mnvp", "parp", "nparp", "scoreplot"),
+  table.placement = "ht",
+  latex.environments = c("center"),
+  booktabs = F,
+  booktabs.cmalign = "l",
+  caption = NULL,
+  label = NULL,
+  col.width = F,
+  digits = 3,
+  pvalformat = pvalf,
+  scoreplot.marks = c(
+    "mark=square*,mark options={color=red},mark size=0.8pt",
+    "mark=*,mark size=0.6pt",
+    "mark=o,mark size=0.7pt"),
+  scoreplot.scale = 6,
+  scoreplot.before = "\\raisebox{-.5\\height}{\\resizebox {1.2cm} {1.2cm} {",
+  scoreplot.after = "}}",
+  ...) {
 
+  ndata <- length(data.show)
+  hlines <- if (booktabs) {
+    list(top = "\\toprule", mid = "\\midrule", bot = "\\bottomrule",
+         c = pst("\\cmidrule(", booktabs.cmalign, ")"))
+  } else {
+    list(top = "\\hline", mid = "\\hline", bot = "\\hline", c = "\\cline")
+  }
 
-    ndata <- length(data.show)
-    hlines <- if (booktabs) {
-      list(top = "\\toprule", mid = "\\midrule", bot = "\\bottomrule",
-           c = pst("\\cmidrule(", booktabs.cmalign, ")"))
-    } else {
-      list(top = "\\hline", mid = "\\hline", bot = "\\hline", c = "\\cline")
-    }
+  nout <- dim(mic)[1]
+  smic <- summary(mic)
 
-    nout <- dim(mic)[1]
-    smic <- summary(mic)
+  ltxtab <- list()
+  idx <- 1
 
-    ltxtab <- list()
-    idx <- 1
+  ltxtab[[idx]] <- pst("\\begin{table}[", table.placement, "]")
+  idx <- idx + 1
 
-    ltxtab[[idx]] <- pst("\\begin{table}[", table.placement, "]")
+  ltxtab[[idx]] <- pst("\\begin{", latex.environments ,"}")
+  idx <- idx + 1
+
+  if (col.width) {
+    ltxtab[[idx]] <- "\\resizebox{\\columnwidth}{!}{%"
+    idx <- idx + 1
+  }
+
+  ltxtab[[idx]] <- pst("\\begin{tabular}{cl", pst(rep("r", nout)), "}")
+  idx <- idx + 1
+
+  ltxtab[[idx]] <- hlines$top
+  idx <- idx + 1
+
+  ltxtab[[idx]] <- pst("\\multirow{2}{*}{Comp.} & \\multirow{2}{*}{Test} & ",
+                       "\\multicolumn{", nout, "}{c}{Outputs} \\\\")
+  idx <- idx + 1
+
+  ltxtab[[idx]] <- pst(hlines$c, "{3-", 2 + nout, "}")
+  idx <- idx + 1
+
+  ltxtab[[idx]] <- pst(" & & ", paste(rownames(mic), collapse = " & ",
+                                      sep = ""),
+                       "\\\\")
+  idx <- idx + 1
+
+  # Cycle through comparisons
+  for (cmp in colnames(mic)) {
+
+    # Put a midrule
+    ltxtab[[idx]] <- hlines$mid
     idx <- idx + 1
 
-    ltxtab[[idx]] <- pst("\\begin{", latex.environments ,"}")
+    # Adjust formatting for specific number of significant digits
+    smicf <- smic[[cmp]]
+
+    # Multi-row with comparison name
+    ltxtab[[idx]] <- pst("\\multirow{", ndata,"}{*}{", cmp, "}")
     idx <- idx + 1
 
-    if (col.width) {
-      ltxtab[[idx]] <- "\\resizebox{\\columnwidth}{!}{%"
+    # Rows with comparison data
+    for (cdata in data.show) {
+      ltxtab[[idx]] <-
+        switch(cdata,
+               npcs = pst(" & $\\#$PCs ",
+                          pst(" & ", as.integer(smicf["#PCs",])), "\\\\"),
+               mnvp = pst(" & MNV ",
+                          pst(" & ", pvalf(unlist(smicf["MNV",]))), "\\\\"),
+               parp = pst(" & $t$-test ",
+                          pst(" & ",
+                              pvalf(unlist(smicf["Par.test",]))), "\\\\"),
+               nparp = pst(" & MW  ",
+                           pst(" & ",
+                               pvalf(unlist(smicf["NonParTest",]))), "\\\\"),
+               scoreplot = pst(" & PCS ",
+                               pst(" & ",
+                                   tscat_apply(mic[, cmp],
+                                               scoreplot.marks,
+                                               scoreplot.scale,
+                                               scoreplot.before,
+                                               scoreplot.after)),
+                               "\\\\"))
       idx <- idx + 1
     }
-
-    ltxtab[[idx]] <- pst("\\begin{tabular}{cl", pst(rep("r", nout)), "}")
-    idx <- idx + 1
-
-    ltxtab[[idx]] <- hlines$top
-    idx <- idx + 1
-
-    ltxtab[[idx]] <- pst("\\multirow{2}{*}{Comp.} & \\multirow{2}{*}{Test} & ",
-                         "\\multicolumn{", nout, "}{c}{Outputs} \\\\")
-    idx <- idx + 1
-
-    ltxtab[[idx]] <- pst(hlines$c, "{3-", 2 + nout, "}")
-    idx <- idx + 1
-
-    ltxtab[[idx]] <- pst(" & & ", paste(rownames(mic), collapse = " & ",
-                                        sep = ""),
-                         "\\\\")
-    idx <- idx + 1
-
-    # Cycle through comparisons
-    for (cmp in colnames(mic)) {
-
-      # Put a midrule
-      ltxtab[[idx]] <- hlines$mid
-      idx <- idx + 1
-
-      # Adjust formatting for specific number of significant digits
-      smicf <- smic[[cmp]]
-
-      # Multi-row with comparison name
-      ltxtab[[idx]] <- pst("\\multirow{", ndata,"}{*}{", cmp, "}")
-      idx <- idx + 1
-
-      # Rows with comparison data
-      for (cdata in data.show) {
-        ltxtab[[idx]] <-
-          switch(cdata,
-                 npcs = pst(" & $\\#$PCs ",
-                            pst(" & ", as.integer(smicf["#PCs",])), "\\\\"),
-                 mnvp = pst(" & MNV ",
-                            pst(" & ", pvalf(unlist(smicf["MNV",]))), "\\\\"),
-                 parp = pst(" & $t$-test ",
-                            pst(" & ",
-                                pvalf(unlist(smicf["Par.test",]))), "\\\\"),
-                 nparp = pst(" & MW  ",
-                             pst(" & ",
-                                 pvalf(unlist(smicf["NonParTest",]))), "\\\\"),
-                 scoreplot = pst(" & PCS ",
-                                 pst(" & ",
-                                     tscat_apply(mic[,cmp], scoreplot.marks,
-                                                 scoreplot.scale)),
-                                 "\\\\"))
-        idx <- idx + 1
-      }
-
-    }
-
-    ltxtab[[idx]] <- hlines$bot
-    idx <- idx + 1
-
-    ltxtab[[idx]] <- "\\end{tabular}"
-    idx <- idx + 1
-
-    if (col.width) {
-      ltxtab[[idx]] <- "} % resize box"
-      idx <- idx + 1
-    }
-
-
-    if (!is.null(caption)) {
-      ltxtab[[idx]] <- pst("\\caption{", caption,"}")
-      idx <- idx + 1
-    }
-
-    if (!is.null(label)) {
-      ltxtab[[idx]] <- pst("\\label{", label,"}")
-      idx <- idx + 1
-    }
-
-    ltxtab[[idx]] <- pst("\\end{", rev(latex.environments) ,"}")
-    idx <- idx + 1
-
-    ltxtab[[idx]] <- "\\end{table}"
-    idx <- idx + 1
-
-    ltxtab <- unlist(ltxtab)
-    class(ltxtab) <- "Latex"
-
-    ltxtab
 
   }
+
+  ltxtab[[idx]] <- hlines$bot
+  idx <- idx + 1
+
+  ltxtab[[idx]] <- "\\end{tabular}"
+  idx <- idx + 1
+
+  if (col.width) {
+    ltxtab[[idx]] <- "} % resize box"
+    idx <- idx + 1
+  }
+
+
+  if (!is.null(caption)) {
+    ltxtab[[idx]] <- pst("\\caption{", caption,"}")
+    idx <- idx + 1
+  }
+
+  if (!is.null(label)) {
+    ltxtab[[idx]] <- pst("\\label{", label,"}")
+    idx <- idx + 1
+  }
+
+  ltxtab[[idx]] <- pst("\\end{", rev(latex.environments) ,"}")
+  idx <- idx + 1
+
+  ltxtab[[idx]] <- "\\end{table}"
+  idx <- idx + 1
+
+  ltxtab <- unlist(ltxtab)
+  class(ltxtab) <- "Latex"
+
+  ltxtab
+
+}
