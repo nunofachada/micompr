8c254f78db42e3cdfb16eebbb42456c57f818d2d
Nuno Fachada <faken@fakenmc.com>
2015-10-22 19:27:53 +0100

    grpoutputs: additional tests and examples

--- R/grpoutputs.R
+++ R/grpoutputs.R
@@ -33,8 +33,20 @@
 #' @export
 #'
 #' @examples
-#' NULL
+#' # Determine paths for data folders, each containing outputs for 10 runs of
+#' # the PPHPC model
+#' dir_nl_ok <- system.file("extdata", "nl_ok", package = "micompr")
+#' dir_jex_ok <- system.file("extdata", "j_ex_ok", package = "micompr")
+#' files <- "stats400v1*.tsv"
 #'
+#' # Create a grouped outputs object using outputs from NetLogo and Java
+#' # implementations of the PPHPC model
+#' go <- grpoutputs(7, c(dir_nl_ok, dir_jex_ok), c(files, files),
+#'                  lvls = c("NL", "JEX"), concat = TRUE)
+#'
+#' # Do the same, but specify output names and don't specify levels
+#' go <- grpoutputs(c("a", "b", "c", "d", "e", "f"),
+#'                  c(dir_nl_ok, dir_jex_ok), c(files, files))
 grpoutputs <- function(outputs, folders, files, lvls = NULL, concat = F, ...) {
 
   # Determine number of file sets (i.e. number of unique factors or levels)
@@ -47,7 +59,7 @@ grpoutputs <- function(outputs, folders, files, lvls = NULL, concat = F, ...) {
     # number of file sets
     if (length(lvls) != nfilesets) {
       stop("Number of file sets is not the same as the given number",
-           " of factors.")
+           " of factor levels.")
     }
 
   } else {
--- tests/testthat/test_grpoutputs.R
+++ tests/testthat/test_grpoutputs.R
@@ -24,10 +24,9 @@ test_that("grpoutputs constructs the expected objects", {
   go_diff <- grpoutputs(outputs, c(dir_nl_ok, dir_jex_diff),
                         c(files, files),
                         lvls = c("NLOK", "JEXDIFF"), concat = T)
-  go_tri <- grpoutputs(outputs[1:6],
+  go_tri <- grpoutputs(6,
                        c(dir_nl_ok, dir_jex_noshuff, dir_jex_diff),
-                       c(files, files, files),
-                       lvls = c("NLOK", "JEXNOSHUF", "JEXDIFF"), concat = F)
+                       c(files, files, files))
 
   #### Start testing ####
 
@@ -36,6 +35,13 @@ test_that("grpoutputs constructs the expected objects", {
   expect_is(go_noshuff, "grpoutputs")
   expect_is(go_diff, "grpoutputs")
   expect_is(go_tri, "grpoutputs")
+
+  # Test if outputs are as expected
+  expect_equal(names(go_ok$data), outputs)
+  expect_equal(names(go_noshuff$data), outputs)
+  expect_equal(names(go_diff$data), outputs)
+  expect_equal(names(go_tri$data),
+               c("out1", "out2", "out3", "out4", "out5", "out6"))
 
   # Test if groups are as expected
   expect_equal(go_ok$groups, c(10, 10))
@@ -47,12 +53,64 @@ test_that("grpoutputs constructs the expected objects", {
   expect_equal(go_ok$lvls, c("NLOK", "JEXOK"))
   expect_equal(go_noshuff$lvls, c("NLOK", "JEXNOSHUF"))
   expect_equal(go_diff$lvls, c("NLOK", "JEXDIFF"))
-  expect_equal(go_tri$lvls, c("NLOK", "JEXNOSHUF", "JEXDIFF"))
+  expect_equal(go_tri$lvls, c(1, 2, 3))
 
   # Test if concat is as expected
   expect_true(go_ok$concat)
   expect_true(go_noshuff$concat)
   expect_true(go_diff$concat)
   expect_false(go_tri$concat)
+
+})
+
+test_that("grpoutputs throws errors when improperly invoked", {
+
+  # OS-specific file separator
+  fs <- .Platform$file.sep
+
+  #### Start testing ####
+
+  # Should throw error because number of levels != number of files
+  expect_error(
+    grpoutputs(4, c("dir1", "dir2"), c("*.tsv"), lvls = c("A", "B")),
+    "Number of file sets is not the same as the given number of factor levels.",
+    fixed = TRUE
+  )
+
+  # Should throw error because no files were found
+  expect_error(
+    grpoutputs(4, "some_fake_folder", c("fake_files*.csv", "also_fakes*.csv")),
+    paste("No files were found: some_fake_folder", fs, "fake_files*.csv",
+          sep = ""),
+    fixed = TRUE
+  )
+
+  # Should throw error because number of specified outputs is larger than
+  # outputs available in file
+  expect_error(
+    grpoutputs(7, system.file("extdata", "nl_ok", package = "micompr"),
+               "stats400v1r1.tsv", lvls = "just_the_one", concat = F),
+    paste("Specified number of outputs is larger than the number ",
+          "of outputs in file '",
+          system.file("extdata", "nl_ok", package = "micompr"), fs,
+          "stats400v1r1.tsv'.", sep = ""),
+    fixed = TRUE
+  )
+
+  # Should throw error because outputs in files have different lengths
+  expect_error(
+    grpoutputs(4, c(system.file("extdata", "nl_ok", package = "micompr"),
+                    system.file("extdata", "testdata", package = "micompr")),
+               c("stats400v1r1.tsv", "stats400v1r1n50.tsv")),
+    paste("Length of outputs in file '",
+          system.file("extdata", "testdata", package = "micompr"),
+          fs, "stats400v1r1n50.tsv",
+          "' does not match the length of outputs in file '",
+          system.file("extdata", "nl_ok", package = "micompr"),
+          fs, "stats400v1r1.tsv",
+          "'.", sep = ""),
+    fixed = TRUE
+  )
+
 
 })
