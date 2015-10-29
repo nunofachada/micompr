0de3e11c407e4b946a4793b5cf5d5c8507a1531b
Nuno Fachada <faken@fakenmc.com>
2015-10-29 21:02:24 +0000

    Add example to toLatex, finish examples and tests

--- R/micomp_latex.R
+++ R/micomp_latex.R
@@ -230,7 +230,18 @@ tscat_apply <- function(cmps, marks, tscale, before = "", after = "") {
 #' @export
 #'
 #' @examples
-#' NULL
+#'
+#' # Create a micomp object, use provided dataset, three first outputs, plus
+#' # a fourth concatenated output
+#' mic <- micomp(4, 0.8,
+#'               list(list(name = "NLOKvsJEXOK", grpout = pphpc_ok),
+#'                    list(name = "NLOKvsJEXNOSHUFF", grpout = pphpc_noshuff),
+#'                    list(name = "NLOKvsJEXDIFF", grpout = pphpc_diff)),
+#'               concat = TRUE)
+#'
+#' # Print latex table source to screen
+#' toLatex(mic)
+#'
 toLatex.micomp <- function(
   mic,
   data.show = c("npcs", "mnvp", "parp", "nparp", "scoreplot"),
