7db650b034b347ddda289c9eed0e880552b40657
Nuno Fachada <faken@fakenmc.com>
2015-10-30 00:43:04 +0000

    Add package level docs (skeleton only)

---
+++ R/micompr.R
@@ -0,0 +1,23 @@
+#' micompr: model-independent comparison of simulation output
+#'
+#' micompr is an R package for model-independent comparison of simulation
+#' output.
+#'
+#' @section Usage:
+#'
+#' To do.
+#'
+#' @references \href{http://arxiv.org/abs/1509.09174}{Model-independent
+#' comparison of simulation output}, arXiv preprint.
+#'
+#' @note MIT License
+#'
+#' @docType package
+#' @name micompr
+NULL
+#> NULL
+
+# Convert this file to README.md using the following instructions:
+# tools::Rd2HTML("man/micompr.Rd", "README.html")
+# rmarkdown::pandoc_convert("README.html", to = "markdown", output="README.md")
+# file.remove("README.html")
