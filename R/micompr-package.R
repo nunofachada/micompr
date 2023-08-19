caf8f3992337ce9cef2913c01182167a5343edfc
Nuno Fachada <faken@fakenmc.com>
2023-08-19 22:21:51 +0100

    Fix packaging issue due to roxygen update

---
+++ R/micompr-package.R
@@ -0,0 +1,9 @@
+# Copyright (c) 2016-2023 Nuno Fachada
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
